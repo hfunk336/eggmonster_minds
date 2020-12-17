2a0aa38e88e8ded5faae74b1e26fe610266458b2
Hunter <hfunk336@west-mec.org>
2020-12-17 14:11:51 -0700

    everything is done

--- list.js
+++ list.js
@@ -2,12 +2,52 @@ var article = document.getElementById("letter")
 
 function change(x){
     article.innerHTML = list[x];
+    
 }
 
 var list = [
-    "Hunter lists", 
-    "Becky list",
-    "Clint list",
-    "Rami list",
-    "Chris List"
+    // hunters list
+    "Dear Santa, I have been very nice this year and 2020 has been very hard. " + 
+    "That is why I want nothing but legos so that I can create my own new world " +
+    "and escape from the troubles of this year. Please bring the requested items: " +
+    "Legos <br> Lego batman <br> Lego superman <br> Lego starwars <br> Lego city <br> " +
+    "Lego indiana jones <br> Lego pirates of the Caribbean <br> Lego beckham <br> " + 
+    "PS5 just for lego games <br> Lego marvel <br> Lego DC <br> Lego transformers <br> Lego Halo <br> Chipotle gift card", 
+
+    // beckys list
+    "Dear Overlord Santa, the first break of falling snow has arrived. The winter winds " +
+    "have finally sweeped in the happy little city Litchfield Park bringing the cheerful " +
+    "atmosphere of Christmas. The season of giving has finally arrived. I can already smell" +
+    "the intoxicating aroma of gingerbread and cookies everywhere and see the illuminating " +
+    "lights of extravagant decorations perched up in front of peoples’ homes. Oh joy the season " + 
+    "of Christmas has finally arrived. I can hear Jack Frost howling above my roof bringing the " +
+    "storming snow and hail to the fierce cold weather outside. All is well in the Le household with " +
+    "the warm air blowing from the vents to the smell of delicious foods being made. For my Christmas list this year, I desire: <br>" +
+    "-144hz monitor <br> good brand new cpu <br> brand new mother board <br> ddr4 ram (16gb)" +
+    "<br> can of compressed air <br> a couple boxes of oreos <br> God of War 2 <br>",
+    
+    // clints list
+    "For Christmas I want only a few things. I know it might be a lot to ask but I have tried" +
+    "my very best to be nice to stay on the nice list. I am sometimes mean to my sister but I " +
+    "always say sorry. I always do what my parents say and always try to help people, and 2020" +
+    "has been a very hard time for everyone… So santa for christmas I want " +
+    "<br> A katana <br> Legos <br> Clothes <br> Shoes <br> Apple Juice <br> Panda and Canes Gift" +
+    "Card <br> Macbook Pro <br> Every Kind of Cereal with Sugar <br> All Candy <br> Fuzzy Socks <br> A Pet Octopus <br>" +
+    "Santa this is all I’m asking for, even a few of these items would be nice. Thank you Santa" ,
+    
+    // rami list
+    "Dear father christmas I have been very good this year, you probably already know this because " +
+    "you’re an all seeing omnipotent being but anyways I want many presents this year and you better " +
+    "not disappoint me like you did last Christmas or else I’ll burn down the North Pole, anywho firstly " +
+    "I would like a racecar, a mansion, the whole continent of Antarctica since no one is using it, and a " +
+    "nintendo switch so i can play splatoon. This is a very minimal list, please make my wish come true",
+
+    // chris list
+    "Dear mr. Clause I know you are probably a very busy man but it would mean a lot if you read this. " +
+    "This year has been very difficult for me because I am now homeless. I am a person with not a lot of " +
+    "needs and I don't want a lot for christmas but if you could get me these few things I would be excited <br>" +
+    "Flying car <br> Light saber <br> Choppa <br> That ric flaire rolly <br> Rami’s offwhite bulletproof vest" +
+    "<br> Gucci crocks <br> Fully flooded ap with big baguettes baby <br> Belenci on my jacket, my shirt my pants " +
+    "and even my undies <br> Rolls royce wraith ghost <br> Cuban link from elliot <br> Otter pop <br> Mclarren drop top <br>" +
+    "If you could get me these few things it would be totally swag. Thanks Santa"
 ];
